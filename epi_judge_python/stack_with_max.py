d191df7b8afdf333a0b2fa381b051e692d75ba82
Fred Kneeland <fred@bladelinux.com>
2021-05-05 10:28:26 -0700

    added solution for stack with max problem

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -3,21 +3,49 @@ from test_framework.test_failure import TestFailure
 
 
 class Stack:
-    def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
+    def __init__(self):
+        self.internal_list = []
+        self.max_stack = []
+        self.max_stack_counts = []
+        self.max_int = None
 
-    def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+    def empty(self):
+        return len(self.internal_list) == 0
 
-    def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+    def max(self):
+        return self.max_int
 
-    def push(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+    def pop(self):
+        temp = self.internal_list.pop()
+        if len(self.internal_list) == 0:
+            self.__init__()
+            return temp
+
+        # if we are popping the max_int we have to update our max_int
+        if temp == self.max_int:
+            # first decrease the counter for that max_int as we may have duplicates
+            self.max_stack_counts[-1] -= 1
+            # if we have no duplicates get the next largest number on the stack
+            if self.max_stack_counts[-1] == 0:
+                self.max_stack_counts.pop()
+                self.max_int = self.max_stack.pop()
+
+        return temp
+
+    def push(self, x):
+        if self.max_int == None or x > self.max_int:
+            # add old max_int to stack
+            self.max_stack.append(self.max_int)
+
+            # update new max_int
+            self.max_int = x
+            
+            # update our counts for this max int
+            self.max_stack_counts.append(1)
+        elif x == self.max_int:
+            self.max_stack_counts[-1] += 1
+
+        self.internal_list.append(x)
 
 
 def stack_tester(ops):
