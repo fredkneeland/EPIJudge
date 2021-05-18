2670450ab8b56f2896a52ff72dbec82578d36592
Fred Kneeland <fred@bladelinux.com>
2021-05-18 16:57:37 -0700

    got individual letter working

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,15 +1,26 @@
 from test_framework import generic_test
 
 
-def is_letter_constructible_from_magazine(letter_text: str,
-                                          magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+def is_letter_constructible_from_magazine(letter_text, magazine_text):
+	# Construct a hash map with a length 26
+	counts = [0 for x in range(26)]
+
+	# Add every letter from letter_text into the
+	for l in magazine_text.lower():
+		counts[ord(l)%26] += 1
+
+	for l in letter_text.lower():
+		idx = ord(l)%26
+		counts[idx] -= 1
+
+		if counts[idx] < 0:
+			return False
+	return True
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            'is_anonymous_letter_constructible.py',
-            'is_anonymous_letter_constructible.tsv',
-            is_letter_constructible_from_magazine))
+	exit(
+		generic_test.generic_test_main(
+			'is_anonymous_letter_constructible.py',
+			'is_anonymous_letter_constructible.tsv',
+			is_letter_constructible_from_magazine))
