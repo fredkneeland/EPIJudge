33e576a8e2ad401e174f69069f2a6e7c5fc59008
Fred Kneeland <fred@bladelinux.com>
2021-05-21 16:30:55 -0700

    solved 3 sums problem

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -4,11 +4,20 @@ from test_framework import generic_test
 
 
 def has_three_sum(A: List[int], t: int) -> bool:
-    # TODO - you fill in here.
-    return True
+	A.sort(reverse=True)
+	return has_sum(A, t, 3)
 
+def has_sum(A, t, count):
+	if count == 1:
+		return t in A
+
+	for i in range(len(A)):
+		if has_sum(A[i:], t-A[i], count-1):
+			return True
+
+	return False
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('three_sum.py', 'three_sum.tsv',
-                                       has_three_sum))
+	exit(
+		generic_test.generic_test_main('three_sum.py', 'three_sum.tsv',
+									   has_three_sum))
