e89a2fe06b47d06bdf6ae011b354618358c0884f
Fred Kneeland <fred@bladelinux.com>
2021-05-21 17:16:28 -0700

    completed the graph problem

--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -11,10 +11,62 @@ WHITE, BLACK = range(2)
 
 Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 
+class Node():
+    def __init__(self, coord, parent):
+        self.coord = coord
+        self.children = []
+        self.parent = parent
+
+    def addChild(self, coord):
+        self.children.append(coord)
 
 def search_maze(maze: List[List[int]], s: Coordinate,
                 e: Coordinate) -> List[Coordinate]:
-    # TODO - you fill in here.
+    newMaze = maze.copy()
+    startNode = Node(s, None)
+    endNode = None
+    nodes = [startNode]
+
+    x_len = len(maze)
+    y_len = len(maze[0])
+
+    x_vals = [-1, 1, 0, 0]
+    y_vals = [0, 0, -1, 1]
+
+    while len(nodes) > 0 and endNode == None:
+        node = nodes.pop()
+
+        for i in range(4):
+            curr_x = node.coord.x+x_vals[i]
+            curr_y = node.coord.y+y_vals[i]
+            if curr_x < 0 or curr_x >= x_len or curr_y < 0 or curr_y >= y_len:
+                continue
+
+            if newMaze[curr_x][curr_y] == 0:
+                new_coord = Coordinate(curr_x, curr_y)
+                newMaze[curr_x][curr_y] = 1
+                new_node = Node(new_coord, node)
+                nodes.append(new_node)
+                node.addChild(new_node)
+                if curr_x == e.x and curr_y == e.y:
+                    endNode = new_node
+                    break
+
+        if endNode != None:
+            break
+
+    if endNode != None:
+        returnVal = [endNode.coord]
+
+        curr_node = endNode.parent
+
+        while curr_node.coord != startNode.coord:
+            returnVal.insert(0, curr_node.coord)
+            curr_node = curr_node.parent
+
+        returnVal.insert(0, startNode.coord)
+        return returnVal
+
     return []
 
 
