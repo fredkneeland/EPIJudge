6469ca7cefd094257f6b46e3ceb8abdea708ca2b
Fred Kneeland <fred@bladelinux.com>
2021-05-23 19:10:07 -0700

    finished power problem

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,10 +2,18 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+	if y == 0:
+		return 1
+		
+	result = x
+	for _ in range(abs(y)-1):
+		result *= x
+
+	if y < 0:
+		return 1 / result
+	return result
 
 
 if __name__ == '__main__':
-    exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
-                                        power))
+	exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
+										power))
